f4af7e6e576841cb7a71bfa479b83e803f1945c9
Julio Navarro <julioocz@gmail.com>
2017-03-18 13:39:35 -0400

    Added prepare_post and post method to the http client. Preparing it for requesting to the authenticated endpoints

--- aiobitfinex/bitfinex_rest.py
+++ aiobitfinex/bitfinex_rest.py
@@ -1,11 +1,21 @@
 import asyncio
 import logging
 import async_timeout
-from typing import Optional, List
+import base64
+import hashlib
+import time
+import hmac
+import json
+from typing import Optional, List, Tuple
 
 import aiohttp
 
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('bitfinex.rest')
+
+
+class NoAPIKeys(Exception):
+    pass
 
 
 class APIPath:
@@ -13,7 +23,7 @@ class APIPath:
 
     API_ROOT = 'https://api.bitfinex.com/v1/{}'
 
-    # -- Public endpoints
+    # -- Public endpoints. http://docs.bitfinex.com/v1/reference
 
     SYMBOLS = API_ROOT.format('symbols')
     SYMBOLS_DETAILS = API_ROOT.format('symbols_details')
@@ -35,24 +45,93 @@ class RESTClient:
     """
 
     def __init__(self,
-                 loop: Optional[asyncio.BaseEventLoop]=None,
-                 session: aiohttp.ClientSession=None):
+                 api_key: Optional[str] = None,
+                 api_secret: Optional[str] = None,
+                 loop: Optional[asyncio.BaseEventLoop] = None,
+                 session: aiohttp.ClientSession = None):
+
         self._loop = loop or asyncio.get_event_loop()
         self._session = session or aiohttp.ClientSession(loop=self._loop)
+
+        # Setting a property `keys` if the keys were provided. They are needed for the private
+        # enpoints
+        if api_key and api_secret:
+            self._api_key = api_key
+            self._api_secret = bytes(api_secret, 'utf-8')
+            self.keys = True
+        else:
+            self.keys = False
+            logger.warning('The API keys was not provided. The authenticated API methdos wont'
+                           ' be available')
 
     def __del__(self):
         if not self._session.closed:
             self._session.close()
+
+    def _prepare_post(self, payload: dict) -> Tuple[any, dict]:
+        """
+        Adds the nonce to the payload. Encodes it with base64. Makes a signature with it
+        and the api key secret. Returning the auth headers that should be used in the request
+
+        :payload: the payload that's going to be send to the private endpoint
+        :return: the headers auth headers that should be send with the request
+        """
+
+        # Adding nonce to the payload
+        payload['nonce'] = str(time.time())
+
+        payload_json = json.dumps(payload)
+        payload_b64 = base64.b64encode(bytes(payload_json, 'utf-8'))
+
+        signature = hmac.new(key=self._api_secret, msg=payload_b64, digestmod=hashlib.sha384)
+        signature = signature.hexdigest()
+
+        headers = {
+            'X-BFX-APIKEY': self._api_key,
+            'X-BFX-PAYLOAD': str(payload_b64, 'utf-8'),
+            'X-BFX-SIGNATURE': signature,
+        }
+        return payload_b64, headers
+
+    async def _post(self, url, payload=None) -> dict:
+        """
+        Performs a POST request, checking if the user is authenticated to create the auth
+        headers. If the user is not authenticated it raises an error.
+        The response code is checkied to se if it's beteween 200 -- 300. If it's, it parses the
+        JSON response to a python object. If not it raises a HTTP erro
+
+        :param url: the api url
+        :param payload: the payload to be send with the request
+        :return: The Bitfinex API response
+        """
+        # Checking keys
+        if not self.keys:
+            raise NoAPIKeys('The private endpoints require the api keys')
+
+        payload = payload or {}
+        payload['request'] = '/v1/account_infos'
+        payload_encoded, headers = self._prepare_post(payload)
+
+        logger.debug(f'Sending post request to {url}')
+        with async_timeout.timeout(20):
+            async with self._session.post(url, data=payload_encoded, headers=headers) as resp:
+                if 200 <= resp.status < 300:
+                    return await resp.json()
+                else:
+                    print(resp.text())
+                    raise aiohttp.errors.HttpProcessingError(
+                        message=f'There was a problem processing {url}', code=resp.status)
 
     async def _fetch(self, url: str) -> any:
         """
         Performs a GET request, checking the response code to see if it's between 200 - 300.
         If it's, it parses the JSON to a python object. If not it raises an HTTP error
 
-        :return: Bitfinex API response
+        :url: the api url
+        :return: The Bitfinex API response
         """
-        logger.debug('Fetching', url)
-        with async_timeout.timeout(10):
+        logger.debug(f'Fetching {url}')
+        with async_timeout.timeout(15):
             async with self._session.get(url) as response:
                 if 200 <= response.status < 300:
                     return await response.json()
@@ -65,8 +144,8 @@ class RESTClient:
         Gets the high level overview of the state of the market (ticker) for the given symbol
         API URL: http://docs.bitfinex.com/v1/reference#rest-public-ticker
 
-        :param symbol: the currency pair that you want information. (btcusd, etc). Yo can look at
-        the available symbols with the symbol method.
+        :param symbol: the currency pair that you want information. (btcusd, etc). Yo can look
+        at the available symbols with the symbol method.
         :return:
         """
         return await self._fetch(APIPath.TICKER.format(symbol))
@@ -129,6 +208,3 @@ class RESTClient:
     async def symbols_details(self) -> List[dict]:
         """Gets a detailed list with the available symbols on the exchange"""
         return await self._fetch(APIPath.SYMBOLS)
-
-
-
